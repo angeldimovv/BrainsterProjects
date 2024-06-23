f12f54cf4ea59dac43ecc13d7f3c0dabebedd7a6
Angel Dimov <dimov.angel2005@gmail.com>
2024-06-23 15:06:35 +0200

    Added AJAX quotable.io API call for random footer book quotes.

--- app/index.php
+++ app/index.php
@@ -115,12 +115,18 @@ session_start();
         </div>
     </main>
     <footer class="position-sticky bottom-0 start-50 v-100 bg-dark text-center">
-        <p class="py-2 mb-0 text-light fs-5">"Those who dare to fail miserably can achieve greatly." - John F. Kennedy</p>
+        <p class="py-2 mb-0 text-light fs-5" id="quote"></p>
     </footer>
+
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <script src="./js/index.js"></script>
 </body>
 
 </html>
