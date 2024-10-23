68441dae39dacc1df88d756e39600f17fa4ae25e
Angel Dimov <dimov.angel2005@gmail.com>
2024-10-23 19:48:10 +0200

    Rewrote README and hard-coded admin credentials

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,11 +4,18 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name' => '',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('admin'),
+        ]);
         User::factory(100)->create();
     }
 }
